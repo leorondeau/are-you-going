2baa1ed2bef49d120762806e3502cb89238e408c
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-08 13:51:17 -0600

    added registration functionality for new user

---
+++ src/components/auth/Register.js
@@ -0,0 +1,94 @@
+import React, { useRef } from "react"
+import "./Login.css"
+
+export const Register = (props) => {
+    const userName = useRef()
+    const email = useRef()
+    const password = useRef()
+    const verifyPassword = useRef()
+    const passwordDialog = useRef()
+    const conflictDialog = useRef()
+
+    const existingUserCheck = () => {
+        // If your json-server URL is different, please change it below!
+        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
+            .then(_ => _.json())
+            .then(user => !!user.length)
+    }
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+
+        if (password.current.value === verifyPassword.current.value) {
+            existingUserCheck()
+                .then((userExists) => {
+                    if (!userExists) {
+                        // If your json-server URL is different, please change it below!
+                        fetch("http://localhost:8088/users", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                email: email.current.value,
+                                password: password.current.value,
+                                name: `${userName.current.value}`
+                            })
+                        })
+                            .then(_ => _.json())
+                            .then(createdUser => {
+                                if (createdUser.hasOwnProperty("id")) {
+                                    // The user id is saved under the key app_user_id in local Storage. Change below if needed!
+                                    localStorage.setItem("ayg__id", createdUser.id)
+                                    props.history.push("/")
+                                }
+                            })
+                    }
+                    else {
+                        conflictDialog.current.showModal()
+                    }
+                })
+        } else {
+            passwordDialog.current.showModal()
+        }
+    }
+
+    return (
+        <main style={{ textAlign: "center" }}>
+
+            <dialog className="dialog dialog--password" ref={passwordDialog}>
+                <div>Passwords do not match</div>
+                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
+            </dialog>
+
+            <dialog className="dialog dialog--password" ref={conflictDialog}>
+                <div>Account with that email address already exists</div>
+                <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
+            </dialog>
+
+            <form className="form--login" onSubmit={handleRegister}>
+                <h1 className="h3 mb-3 font-weight-normal">Please Register for Application Name</h1>
+                <fieldset>
+                    <label htmlFor="userName"> User Name </label>
+                    <input ref={userName} type="text" name="userName" className="form-control" placeholder="user name" required autoFocus />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="inputEmail"> Email address </label>
+                    <input ref={email} type="email" name="email" className="form-control" placeholder="Email address" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="inputPassword"> Password </label>
+                    <input ref={password} type="password" name="password" className="form-control" placeholder="Password" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="verifyPassword"> Verify Password </label>
+                    <input ref={verifyPassword} type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required />
+                </fieldset>
+                <fieldset>
+                    <button type="submit"> Sign in </button>
+                </fieldset>
+            </form>
+        </main>
+    )
+}
+
