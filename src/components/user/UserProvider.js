ad4e8d64f6d0f967cda76e490c611b6936e35348
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-08 13:52:14 -0600

    added functions for get, add, delete for users

---
+++ src/components/user/UserProvider.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+
+export const UserContext = React.createContext()
+
+export const UserProvider = (props) => {
+    const [users , setUsers] = useState([])
+
+    const getUsers = () => {
+        return fetch("http://localhost:8088/users")
+        .then(res => res.json())
+        .then(setUsers)
+    }
+
+    const addUser = User => {
+        return fetch("http://localhost:8088/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(User)
+            })
+            .then(getUsers)
+    }
+
+    const deleteUser = userid => {
+        return fetch(`http://localhost:8088/users/${userid}`, {
+            method: "DELETE",
+        })
+        .then(getUsers)
+
+    }
+
+    return (
+        <UserContext.Provider value = {{
+            Users , addUser , getUsers , releaseUser
+        }}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
