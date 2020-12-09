e17a3a9f6dec008c808fbd94d5223e7b3e5cdff9
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-08 22:59:43 -0600

    Have partial form rendering and posting partial data in EventForm module

--- src/components/Event/EventForm.js
+++ src/components/Event/EventForm.js
@@ -1,17 +1,18 @@
 import React, { useContext, useState, useEffect } from "react"
 import { EventContext } from "./EventProvider"
-import { UserContext } from "./user/UserProvider"
+
+// import { UserContext } from "../user/UserProvider"
 
 export const EventForm = (props) => {
     // Use the required context providers for data
-    const { users, getUser } = useContext(UserContext)
+    // const { users, getUsers } = useContext(UserContext)
     const { addEvent, events, updateEvent, getEvents } = useContext(EventContext)
     
     // Component state
-    const [animal, setAnimal] = useState({})
+    const [event, setEvent] = useState({})
 
     // Is there a a URL parameter??
-    const editMode = props.match.params.hasOwnProperty("animalId")
+    const editMode = props.match.params.hasOwnProperty("eventId")
     
     // Has to be defined in the component
     const handleControlledInputChange = (event) => {
@@ -20,145 +21,107 @@ export const EventForm = (props) => {
         and change state instead of modifying current one
         */
        // Adding key value pairs to the object through each from input
-        const newAnimal = Object.assign({}, animal)
-        newAnimal[event.target.name] = event.target.value
-        setAnimal(newAnimal)
+        const newEvent = Object.assign({}, event)
+        newEvent[event.target.name] = event.target.value
+        setEvent(newEvent)
     }
 
     /*
         If there is a URL parameter, then the user has chosen to
-        edit an animal.
+        edit an event.
             1. Get the value of the URL parameter.
-            2. Use that `id` to find the animal.
+            2. Use that `id` to find the event.
             3. Update component state variable.
     */
 
-    // This function fills the form with associated animal to be edited based on the id.
-    const getAnimalInEditMode = () => {
+    // This function fills the form with associated event to be edited based on the id.
+    const getEventInEditMode = () => {
         if (editMode) {
-            const animalId = parseInt(props.match.params.animalId)
-            const selectedAnimal = animals.find(a => a.id === animalId) || {}
-            setAnimal(selectedAnimal)
+            const eventId = parseInt(props.match.params.eventId)
+            const selectedEvent = event.find(e => e.id === eventId) || {}
+            setEvent(selectedEvent)
         }
     }
 
-    // Get animals from API when component initializes
+    // Get events from API when component initializes
     useEffect(() => {
-        getAnimals()
-        getLocations()
-        getCustomers()
+        getEvents()
+        // getUsers()
     }, [])
 
-    // Once provider state is updated, determine the animal (if edit)
+    // Once provider state is updated, determine the event (if edit)
     useEffect(() => {
-        getAnimalInEditMode()
-    }, [animals])
+        getEventInEditMode()
+    }, [events])
 
 
-    const constructNewAnimal = () => {
-        const locationId = parseInt(animal.locationId)
-
-        if (locationId === 0) {
+    const constructNewEvent = () => {
+        const userId = parseInt(localStorage.getItem("ayg__id"))
+        const name = event.name
+        const location = event.location
+   
+        if (location === "" ) {
             window.alert("Please select a location")
         } else {
             if (editMode) {
-                updateAnimal({
-                    id: animal.id,
-                    name: animal.name,
-                    breed: animal.breed,
-                    locationId: locationId,
-                    treatment: animal.treatment,
-                    customerId: parseInt(localStorage.getItem("kennel_customer"))
+                updateEvent({
+                    id: event.id,
+                    name,
+                    location,
+                    // date: event.date,
+                    // details: event.details,
+                    // time: event.time,
+                    userId
                 })
-                    .then(() => props.history.push("/animals"))
+                    .then(() => props.history.push("/"))
             } else {
-                addAnimal({
-                    name: animal.name,
-                    breed: animal.breed,
-                    locationId: locationId,
-                    treatment: animal.treatment,
-                    customerId: parseInt(localStorage.getItem("kennel_customer"))
+                addEvent({
+                    name,
+                    location,
+                    // date: event.date,
+                    // details: event.details,
+                    // time: event.time,
+                    userId
                 })
-                    .then(() => props.history.push("/animals"))
+                    .then(() => props.history.push("/"))
             }
         }
     }
 
     return (
-        <form className="animalForm">
-            <h2 className="animalForm__title">{editMode ? "Update Animal" : "Admit Animal"}</h2>
+        <form className="">
+            <h2 className="eventForm__title">{editMode ? "Update Event" : "Submit Event"}</h2>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="name">Animal name: </label>
+                    <label htmlFor="name">Event name: </label>
                     <input type="text" name="name" required autoFocus className="form-control"
                         proptype="varchar"
-                        placeholder="Animal name"
-                        value={animal.name}
+                        placeholder="Event name"
+                        value={event.name}
                         onChange={handleControlledInputChange}
                     />
+                    {console.log("eventName" , event.name)}
                 </div>
             </fieldset>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="breed">Animal breed: </label>
-                    <input type="text" name="breed" required className="form-control"
+                    <label htmlFor="location">Event location: </label>
+                    <input type="text" name="location" required autoFocus className="form-control"
                         proptype="varchar"
-                        placeholder="Animal breed"
-                        value={animal.breed}
+                        placeholder="Event location"
+                        value={event.location}
                         onChange={handleControlledInputChange}
-                    />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="locationId">Location: </label>
-                    <select name="locationId" className="form-control"
-                        proptype="int"
-                        value={animal.locationId}
-                        onChange={handleControlledInputChange}>
-
-                        <option value="0">Select a location</option>
-                        {locations.map(e => (
-                            <option key={e.id} value={e.id}>
-                                {e.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </fieldset>
-            <fieldset>
-                    <div className="form-group">
-                        <label htmlFor="customerId">Caretaker</label>
-                        <select name="customerId" className="form-control"
-                            proptype="int"
-                            value={animal.customerId}
-                            onChange={handleControlledInputChange}>
-                            <option value="0">Select a caretaker</option>
-                            {customers.map(e => (
-                                <option key={e.id} value={e.id}>
-                                    {e.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="treatment">Treatments: </label>
-                    <textarea type="text" name="treatment" className="form-control"
-                        proptype="varchar"
-                        value={animal.treatment}
-                        onChange={handleControlledInputChange}>
-                    </textarea>
+                        />
+                        {console.log("eventLocation" , event.location)}
                 </div>
             </fieldset>
             <button type="submit"
                 onClick={evt => {
                     evt.preventDefault()
-                    constructNewAnimal()
+                    constructNewEvent()
                 }}
                 className="btn btn-primary">
-                {editMode ? "Save Updates" : "Make Reservation"}
+                {editMode ? "Save Updates" : "Save Event"}
             </button>
         </form>
     )
