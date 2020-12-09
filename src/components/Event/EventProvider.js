babdabc82cfad4784293cea839d6dc91f33fbd38
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-08 23:00:25 -0600

    added functions that fetch relative event data from db

---
+++ src/components/Event/EventProvider.js
@@ -0,0 +1,69 @@
+import React, { useState , useEffect } from "react"
+
+export const EventContext = React.createContext()
+
+export const EventProvider = (props) => {
+    const [events , setEvents] = useState([])
+    const [ searchTerms , setSearchTerms ] = useState("") 
+
+    const getEvents = () => {
+        return fetch("http://localhost:8088/events")
+            .then(res => res.json())
+            .then(setEvents)
+    }
+
+    // Use expand when source has foreign key
+    // Use embed when no foreign key.
+    // If uncertain embed will give empty array if no fk
+    // One to many relationship, embed
+    const getEventById = (id) => {
+        return fetch (`http://localhost:8088/events/${ id }`)
+            .then(res => res.json())
+    }
+
+
+    const addEvent = event => {
+        return fetch("http://localhost:8088/events" , {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(event)
+        })
+        .then(getEvents)
+    }  
+    const deleteOwnerEvent = event => {
+        return fetch (`http://localhost:8088/events/${event.id}`, {
+            method: "DELETE"
+        })
+        .then(getEvents)
+    }
+
+    const deleteUsersEvent = eventid => {
+        return fetch (`http://localhost:8088/events/${eventid}`, {
+            method: "DELETE"
+        })
+        .then(getEvents)
+    }
+
+    // Whenever altering an existing entity the fetch url must have the id
+    const updateEvent = event => {
+        return fetch (`http://localhost:8088/events/${event.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(event)
+        })
+        .then(getEvents)
+    }
+
+    return (
+        <EventContext.Provider value={{
+            events , addEvent , getEvents , getEventById , searchTerms , setSearchTerms , deleteOwnerEvent, deleteUsersEvent ,updateEvent
+        }}>
+            {props.children}
+        </EventContext.Provider>
+    )
+
+    }
