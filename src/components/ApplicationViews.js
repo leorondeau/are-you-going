3d95a50c0343a9f9e9ae6a9b148d87cceca5c807
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-10 15:22:03 -0600

    added EventList and route to detail

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,17 +2,29 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { EventProvider } from "./event/EventProvider"
 import { EventForm } from "./event/EventForm"
+import { EventList } from "./event/EventList"
+import { UserProvider } from "./user/UserProvider"
+// import { EventHomeDetail } from './event/EventHomeDetail'
 
-// import { UserProvider } from "./user/UserProvider"
+
+
 
 export const ApplicationViews = (props) => {
     return (
         <>
-            <EventProvider>
-                <Route exact path="/events" render={
-                    props => <EventForm {...props} />
-                } />
-            </EventProvider>
+            <UserProvider>
+                <EventProvider>
+                    <Route exact path="/events" render={
+                        props => <EventForm {...props} />
+                    } />
+                    <Route exact path="/" render={
+                        props => <EventList {...props} />
+                    } />
+                    {/* <Route path="/events/:eventId(\d+)" render={
+                        props => <EventHomeDetail {...props}/>
+                    }/> */}
+                </EventProvider>
+            </UserProvider>
         </>
     )
 }
