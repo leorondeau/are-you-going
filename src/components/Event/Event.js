e656fbe8c7bf841fd4dd839d32db11d40f067eb8
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-11 10:45:16 -0600

    added button functionality so that when clicked active user is added to usersEvents join table

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React , { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { UserEventContext } from '../user/UsersEventsProvider'
 
 
 
-export const Event = ({ event , user} ) => (
-
-        <section className="event">
-            <h3 className="event__name">
-                <Link to={`/events/${event.id}`}>
-                    {event.name}
-                </Link>
-            </h3>
-            <div className="event__location">{event.location}</div>
-            <div className="event__date">{event.startDate}</div>
-            <div className="event__details">{event.details}</div>
-            <div className="event__creator">b: {user.name}</div>
-            
-            
-            <button type="button" class="btn btn-info">Yes</button>
-        </section>
+export const Event = ({ event, user }) => {
+    const userId = parseInt(localStorage.getItem("ayg__id"))
     
-)
+    const { addUsersEvents } = useContext(UserEventContext)
+    return (
+        <div>
+            <section className="event">
+                <h3 className="event__name">
+                    <Link to={`/events/${event.id}`}>
+                        {event.name}
+                    </Link>
+                </h3>
+                <div className="event__location">{event.location}</div>
+                <div className="event__date">{event.startDate}</div>
+                <div className="event__details">{event.details}</div>
+                <div className="event__creator">by: {user.name}</div>
+                <button type="button" onClick={
+                    () => addUsersEvents({
+                        eventId: event.id,
+                        userId
+                    })
+
+                   
+                }>Yes</button>
+            </section>
+        </div>
+        )
+        }
