90ef8e3bde31cc84d5bb315658527a0bcd4e0543
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-15 17:53:07 -0600

    added link to name so when it's clicked it goes back to that user's profile

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 export const User = ({ user }) => (
     // {console.log("USER" , user.name)}
     <section className="user">
-        <h3 className="user__name">
-            User: <Link to={`/profile/user/${user.id}`}> {user.name}</Link>
+        <h4 className="user__name">
+            <Link to={`/profile/user/${user.id}`}> {user.name}</Link>
 
-        </h3>
+        </h4>
     </section>
 
 )
