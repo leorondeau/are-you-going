bc3a7622eeb7ecbfc86720e219c7f023c5f1b708
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-14 18:31:55 -0600

    this has the listing event functinality along with ProfileGoEvent

--- src/components/profile/ProfileEventList.js
+++ src/components/profile/ProfileEventList.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { EventContext } from '../event/EventProvider'
-import { UserContext } from '../user/UserProvider'
 import { UserEventContext } from '../user/UsersEventsProvider'
-
+import { ProfileGoEvent } from './ProfileGoEvent'
 
 
 
@@ -12,27 +11,38 @@ export const ProfileEventList = (props) => {
     const activeUserId = parseInt(localStorage.getItem("ayg__id"))
 
     const { events, getEvents } = useContext(EventContext)
-    const { users, getUsers } = useContext(UserContext)
-    const { usersEvents, addUsersEvents, deleteUsersEvent, getUsersEvents } = useContext(UserEventContext)
+    const { usersEvents, getUsersEvents } = useContext(UserEventContext)
+
+    const [selectedEvent, setSelectedEvent] = useState([])
+    const [eventName, setEventName] = useState([])
 
     useEffect(() => {
+
         getUsersEvents()
             .then(getEvents)
-        // console.log("usersEvents", usersEvents)
 
     }, [])
-    const activeUserEvents = usersEvents.filter(ue => ue.userId === activeUserId)
-    console.log("activeUserEvents", activeUserEvents)
+
+
+    useEffect(() => {
+        const activeUserEvents = usersEvents.filter(ue => ue.userId === activeUserId) || {}
+        setSelectedEvent(activeUserEvents)
+
+        const eventNames = activeUserEvents.map(aue => {
+            return events.find(eve => aue.eventId === eve.id)
+        }) || {}
+        setEventName(eventNames)
+
+    }, [events , usersEvents ])
 
     return (
         <>
             <div className="events">
                 <article className="eventList">
+                    <h2>Going List</h2>
                     {
-
-                        <div></div>
+                        eventName.map(en => <ProfileGoEvent key={en.id} event={en} />)
                     }
-
                 </article>
             </div>
         </>
